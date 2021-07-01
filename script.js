19cb0e5742bbbd58bb3457358a42a7738a7e6209
Christa Soliven <xtasoliven@gmail.com>
2021-07-01 01:58:29 -0700

    likes

---
+++ script.js
@@ -0,0 +1,19 @@
+var x = 0;
+document.getElementById("plus").innerHTML = x;
+function add() {
+    document.getElementById("plus").innerHTML = ++x;
+}
+
+
+var x = 9;
+document.getElementById("plus").innerHTML = x;
+function add() {
+document.getElementById("plus").innerHTML = ++x;
+}
+
+
+var x =4;
+document.getElementById("plus").innerHTML = x;
+function add() {
+    document.getElementById("plus").innerHTML = ++x;
+}
